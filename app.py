32e92eafafac4ae8230f14e095ad3a4e3596a365
Vai2839 <143083251+Vai2839@users.noreply.github.com>
2025-01-21 20:58:23 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+
+import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd
+
+import joblib
+
+model = pickle.load(open('model.pkl','rb'))
+# model = joblib.load('model.joblib')
+
+st.title('Credit Card Fraud Detection')
+input_val=st.text_input('Enter all required feature values')
+input_val=input_val.split(',')
+
+submit=st.button("Submit")
+
+if submit:
+    val=np.asarray(input_val,dtype=np.float64)
+    val = val.reshape(1, -1)
+    pred=model.predict(val)
+    if pred==0:
+        st.write("Legitimate transaction")
+    else:
+        st.write("Fraud")
